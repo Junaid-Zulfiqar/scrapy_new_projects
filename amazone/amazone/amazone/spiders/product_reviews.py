11ddbc23d9a733940304b13eac66a8acd747e625
Junaid-Zulfiqar <junaidzulfiqar377@gmail.com>
2021-10-06 22:06:25 +0500

    amazone project from one page to another

---
+++ amazone/amazone/amazone/spiders/product_reviews.py
@@ -0,0 +1,41 @@
+import scrapy
+
+
+class ProductReviewsSpider(scrapy.Spider):
+    name = 'product_reviews'
+    allowed_domains = ['www.amazon.com']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.amazon.com/s?k=lighting/', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
+        })
+
+    def parse(self, response):
+        products = response.xpath("//span/div/div[@class='a-section a-spacing-medium']")
+        for product in products:
+            link = product.xpath(".//span/a/@href").get()
+            rating = product.xpath(".//div[2]/div[2]/div/span/@aria-label").get()
+            rating_split = rating.split(" ")
+            value = float(rating_split[0])
+            if value > 4.5:
+                yield response.follow(url = link, callback = self.parse_details,meta = {"rating":rating})
+            else:
+                pass    
+
+            # product_url = response.urljoin(link)
+            # yield {"product_url":product_url,
+            #         "rating":rating}
+
+    def parse_details(self,response):
+        rating = response.request.meta['rating']
+        title = response.xpath("//span[@id='productTitle']/text()").get()
+        title = title.replace("\n","")
+        price = response.xpath("//span[@id='priceblock_ourprice']/text()").get()
+        global_rating = response.xpath("//span[@id='acrCustomerReviewText']/text()").get()    
+
+        yield {
+            "product_rating":rating,
+            "product_title":title,
+            "product_price":price,
+            "global_rating":global_rating
+        }           
