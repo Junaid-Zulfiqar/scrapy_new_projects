4c497d91a8ab0a1b6d17463f092503a6ead3e85f
Junaid-Zulfiqar <junaidzulfiqar377@gmail.com>
2021-10-12 17:28:46 +0500

    books information get using json

---
+++ qoates/qoates/spiders/libray.py
@@ -0,0 +1,27 @@
+import scrapy
+import json
+from scrapy.exceptions import CloseSpider
+
+
+
+
+class LibraySpider(scrapy.Spider):
+    name = 'library'
+    allowed_domains = ['openlibrary.org']
+
+    incremented_by = 12
+    offset = 0
+    start_urls = ['https://openlibrary.org/subjects/picture_books.json?limit=12&offset=12']
+
+    def parse(self, response):
+        if response.status == 500:
+            return CloseSpider
+        resp = json.loads(response.body)
+        works = resp.get('works')
+        for count in works:
+           yield {
+               "title":count.get("title"),
+               "subject":count.get("subject")
+           }
+        self.offset +=self.incremented_by   
+        yield scrapy.Request(url = f'https://openlibrary.org/subjects/picture_books.json?limit=12&offset={self.offset}',callback = self.parse)
